7e8fd7571da7129d8655a62aaef006cef746b2aa
hamed.hp <hamed.hp.86@gmail.com>
2021-11-26 22:11:51 +0330

    top algorithm

---
+++ top one.py
@@ -0,0 +1,24 @@
+def top(arr):
+    values ={}
+    result = []
+    f_val = 0
+
+    for i in arr :
+        if i in values:
+            values[i] += 1
+        else:
+            values[i] = 1 
+
+    f_val = max(values.values())
+
+    for i in values.keys():
+        if values[i] == f_val:
+            result.append(i)
+        else:
+            continue
+
+    return result
+
+
+
+print(top([4,1,2,3,2,3,2,]))
